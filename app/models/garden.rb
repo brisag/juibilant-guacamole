40e2676825a78cad4efdb611f5fe3b049321dea0
Brisa Garcia <brisagarciaglz@gmail.com>
2021-04-27 11:32:50 -0600

    Add models, relationships, and depdneant destroy

--- app/models/garden.rb
+++ app/models/garden.rb
@@ -1,3 +1,11 @@
 class Garden < ApplicationRecord
   has_many :plots
+  has_many :plot_plants, through: :plots
+  has_many :plants, through: :plot_plants
+
+  def unique_fast_plants
+    plants
+    .where('days_to_harvest < ?', 90)
+    .distinct
+  end
 end
