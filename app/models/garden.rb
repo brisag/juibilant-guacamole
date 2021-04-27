c245eba31bb61c3cf9c8e73dfd5b0395b64d3b8a
Brisa Garcia <brisagarciaglz@gmail.com>
2021-04-27 11:58:13 -0600

    Add extesnions AR

--- app/models/garden.rb
+++ app/models/garden.rb
@@ -3,9 +3,26 @@ class Garden < ApplicationRecord
   has_many :plot_plants, through: :plots
   has_many :plants, through: :plot_plants
 
-  def unique_fast_plants
+  # def unique_fast_plants
+  #   plants
+  #   .where('days_to_harvest < ?', 90)
+  #   .distinct
+  # end
+
+  def unique_fast_plants_sorted_by_most_popular
     plants
+    .select("plants.*, count(plants) as plant_count")
     .where('days_to_harvest < ?', 90)
+    .group(:id)
+    .order(plant_count: :desc)
     .distinct
   end
+
+
+  # def popular_plants
+  #   plants.select("plants.*, count(plants) as plant_count")
+  #   .group(:id)
+  #   .order(plant_count: :desc)
+  #   .distinct
+  # end
 end
